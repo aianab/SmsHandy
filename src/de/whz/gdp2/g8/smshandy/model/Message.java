8df8f6672d5ac277c8656bff7cecda0715f49cb0
aib18j35 <aiana.baitakova.j35@fh-zwickau.de>
2019-05-03 18:24:28 +0200

    add Message and TariffPlan

--- src/de/whz/gdp2/g8/smshandy/model/Message.java
+++ src/de/whz/gdp2/g8/smshandy/model/Message.java
@@ -1,5 +1,57 @@
 package de.whz.gdp2.g8.smshandy.model;
 
-public class Message {
+import java.util.Date;
 
+public class Message extends SmsHandy{
+	private String content;
+	private Date date;
+	private String from;
+	private String to;
+	
+	public Message() {
+		
+	}
+	
+	public Message(String from, String to, String content, Date date ) {
+		this.content = content;
+		this.from = from;
+		this.to = to;
+		this.date = date;
+	}
+
+	public String toString() {
+		return " ";
+	}
+	
+	public Date getDate() {
+		return this.date;
+	}
+	
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	
+	public String getContent() {
+		return this.content;
+	}
+	
+	public void setContent(String content) {
+		this.content = content;
+	}
+	
+	public String getTo() {
+		return this.to;
+	}
+	
+	public void setTo(String to) {
+		this.to = to;
+	}
+	
+	public String getFrom() {
+		return this.from;
+	}
+	
+	public void setFrom(String from) {
+		this.from = from;
+	}
 }
--- src/de/whz/gdp2/g8/smshandy/model/TariffPlanSmsHandy.java
+++ src/de/whz/gdp2/g8/smshandy/model/TariffPlanSmsHandy.java
@@ -1,5 +1,22 @@
 package de.whz.gdp2.g8.smshandy.model;
 
-public class TariffPlanSmsHandy {
+public class TariffPlanSmsHandy extends SmsHandy {
+	
+private int remainingFreeSms = 100;
 
+public TariffPlanSmsHandy(String number, Provider provider) {
+	super(number, provider);
+}
+
+public boolean canSendSms() {
+	return true;
+}
+
+public void payForSms() {
+	
+}
+
+public int getRemainingFreeSms() {
+	return 0;
+}
 }
