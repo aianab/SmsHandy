2e6a2012a8a766e17373f2477e8c94d301b2e294
aib18j35 <aiana.baitakova.j35@fh-zwickau.de>
2019-05-06 19:47:41 +0200

    Sms handy corrected

--- src/de/whz/gdp2/g8/smshandy/model/SmsHandy.java
+++ src/de/whz/gdp2/g8/smshandy/model/SmsHandy.java
@@ -6,8 +6,6 @@ import java.util.List;
 
 public abstract class SmsHandy {
 	private String number;
-	private List<Message> received;
-	private List<Message> sent;
 	
 	private List<Message> sent;
 	
@@ -17,17 +15,10 @@ public abstract class SmsHandy {
 	
 	public SmsHandy(String number, Provider provider) {
 		this.number = number;
-		this.provider = provider;
 	}
 	
 	public void sendSms(String to, String content) {
-		Message message = new Message();
-		message.setFrom(number);
-		message.setTo(to);
-		message.setContent(content);
-		message.setDate(new Date());
-		provider.send(message);
-		sent.add(message);
+		
 	}
 	
 	public abstract boolean canSendSms();
@@ -64,11 +55,7 @@ public abstract class SmsHandy {
 	}
 	
 	public void receiveSms(Message message) {
-<<<<<<< HEAD
 	
-=======
-		received.add(message);
->>>>>>> 75e83e9420f0bd294fc58fd90560d21fd53a323b
 	}
 	
 }
