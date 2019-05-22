ae96ab5e0edfc0a3ef3759325150942d10103067
Murat <murat.ruslan.uulu.j38@fh-zwickau.de>
2019-05-22 18:37:05 +0200

    Add NumberNotGivenException

---
+++ src/de/whz/gdp2/g8/smshandy/exception/NumberNotGivenException.java
@@ -0,0 +1,7 @@
+package de.whz.gdp2.g8.smshandy.exception;
+
+public class NumberNotGivenException extends Exception {
+	public NumberNotGivenException() {
+		super("Number should be given");
+	}
+}
