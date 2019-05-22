0884ef8231704f364a6483f0285bbb1013df6b02
aib18j35 <aiana.baitakova.j35@fh-zwickau.de>
2019-05-22 18:38:45 +0200

    updated: SmsHandy class

--- src/de/whz/gdp2/g8/smshandy/exception/ProviderNotGivenException.java
+++ src/de/whz/gdp2/g8/smshandy/exception/ProviderNotGivenException.java
@@ -1,5 +1,7 @@
 package de.whz.gdp2.g8.smshandy.exception;
 
-public class ProviderNotGivenException {
-
+public class ProviderNotGivenException extends Exception{
+	public ProviderNotGivenException(){
+		super("Provider not given");
+	}
 }
