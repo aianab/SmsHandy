48202f39404050d8a18633460f7273d729f10b50
Murat <murat.ruslan.uulu.j38@fh-zwickau.de>
2019-05-26 18:35:56 +0200

    add: buttons

--- src/de/whz/gdp2/g8/smshandy/ProviderListController.java
+++ src/de/whz/gdp2/g8/smshandy/ProviderListController.java
@@ -1,5 +1,6 @@
 package de.whz.gdp2.g8.smshandy;
 
+=
 import java.util.Arrays;
 import java.util.List;
 
@@ -7,11 +8,15 @@ import de.whz.gdp2.g8.smshandy.model.Provider;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 
 public class ProviderListController {
 	@FXML
 	private ListView<Provider> providerListView;
+	
+	@FXML
+	private Button newButton;
 	
 	public ProviderListController() {
 
@@ -23,6 +28,10 @@ public class ProviderListController {
 		initProviders();
 		list.addAll(Provider.providerList);
 		providerListView.setItems(list);
+		
+		newButton.setOnMouseClicked(e -> {
+			
+		});
 	}
 	
 	
@@ -30,5 +39,9 @@ public class ProviderListController {
 		new Provider("Beeline");
 		new Provider("O!");
 		new Provider("Megacom");
+	}
+	
+	private void showAddNewProviderWindow() {
+		
 	}
 }
