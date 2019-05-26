68a6f4c236bd8b60eac8c96ddc20a14ed1ff28fd
Murat <murat.ruslan.uulu.j38@fh-zwickau.de>
2019-05-26 13:00:15 +0200

    add: main class

---
+++ src/de/whz/gdp2/g8/smshandy/Main.java
@@ -0,0 +1,52 @@
+package de.whz.gdp2.g8.smshandy;
+
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+	
+	private Stage primaryStage;
+    private BorderPane rootLayout;
+    
+    public static void main(String[] args) {
+		launch(args);
+	}
+
+    @Override
+    public void start(Stage primaryStage) {
+        this.primaryStage = primaryStage;
+        this.primaryStage.setTitle("AddressApp");
+
+        initRootLayout();
+        showFirstLayout();
+    }
+    
+    /**
+     * Initializes the root layout.
+     */
+    public void initRootLayout() {
+        try {
+            // Load root layout from fxml file.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(Main.class.getResource("view/rootLayout.fxml"));
+            rootLayout = (BorderPane) loader.load();
+            
+            // Show the scene containing the root layout.
+            Scene scene = new Scene(rootLayout);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void showFirstLayout() {
+    	
+    }
+	
+}
