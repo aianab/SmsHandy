3a363d8b0d2a89ac46146002886755236f2a1a95
Murat <murat.ruslan.uulu.j38@fh-zwickau.de>
2019-05-27 00:57:23 +0200

    complete: change Provider function

--- src/de/whz/gdp2/g8/smshandy/ListOfSmsHandysController.java
+++ src/de/whz/gdp2/g8/smshandy/ListOfSmsHandysController.java
@@ -1,6 +1,6 @@
 package de.whz.gdp2.g8.smshandy;
 
-
+import java.io.IOException;
 import java.util.List;
 
 import de.whz.gdp2.g8.smshandy.exception.NumberExistsException;
@@ -13,54 +13,83 @@ import de.whz.gdp2.g8.smshandy.model.TariffPlanSmsHandy;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
 public class ListOfSmsHandysController {
-	
+
 	@FXML
 	private Label providerName;
 	@FXML
 	private Button addNewPhoneButton;
 	@FXML
 	private Button removePhoneButton;
+
+	@FXML
+	private Button showSmsHandyInfoButton;
+
 	@FXML
 	private ListView<SmsHandy> listSmsHandysView;
-	
+
 	private Provider provider;
 	private Main mainClass;
 	private ObservableList<SmsHandy> list;
-	
-	
+
 	public ListOfSmsHandysController() {
-		
+
 	}
-	
+
 	public void setProvider(Provider provider) {
 		this.provider = provider;
 		list = FXCollections.observableArrayList();
 		list.setAll(provider.getPhones());
 		listSmsHandysView.setItems(list);
 	}
-	
+
 	public void setMainClass(Main main) {
 		this.mainClass = main;
 	}
-	
+
 	@FXML
 	private void initialize() {
 		removePhoneButton.setOnMouseClicked(e -> {
 			deletePhone(listSmsHandysView.getSelectionModel().getSelectedItem());
 		});
+		
+		showSmsHandyInfoButton.setOnMouseClicked(e -> {
+			showSmsHandyInfo();
+		});
 	}
-	
+
 	private void deletePhone(SmsHandy phone) {
 		provider.removeSmsHandy(phone.getNumber());
 		list.remove(phone);
 	}
-	
-	
+
+	private void showSmsHandyInfo() {
+		try {
+			Stage primaryStage = mainClass.getPrimaryStage();
+			primaryStage.close();
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(Main.class.getResource("view/SmsHandyInfo.fxml"));
+
+			AnchorPane smsHandyInfo = (AnchorPane) loader.load();
+
+			SmsHandyInfoController controller = loader.getController();
+			controller.setSmsHandy(listSmsHandysView.getSelectionModel().getSelectedItem());
+			controller.setMainClass(mainClass);
+			
+			mainClass.setRootPane(smsHandyInfo);
+			primaryStage.show();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }
