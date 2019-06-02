61852441dba9fe810112d0bc87a2ccf7fe938ace
aib18j35 <aiana.baitakova.j35@fh-zwickau.de>
2019-06-02 16:41:28 +0200

    added: confirmateion alert to method removePhoneButton in class  ListOfSmsHandysController

--- src/de/whz/gdp2/g8/smshandy/ListOfSmsHandysController.java
+++ src/de/whz/gdp2/g8/smshandy/ListOfSmsHandysController.java
@@ -2,6 +2,8 @@ package de.whz.gdp2.g8.smshandy;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Optional;
+
 
 import de.whz.gdp2.g8.smshandy.exception.NumberExistsException;
 import de.whz.gdp2.g8.smshandy.exception.NumberNotExistException;
@@ -14,17 +16,23 @@ import de.whz.gdp2.g8.smshandy.model.TariffPlanSmsHandy;
 import de.whz.gdp2.g8.smshandy.util.AlertUtil;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonBar.ButtonData;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.Dialog;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.layout.AnchorPane;
 
 import javafx.scene.layout.BorderPane;
@@ -102,8 +110,18 @@ public class ListOfSmsHandysController {
 	}
 
 	private void deletePhone(SmsHandy phone) {
-		provider.removeSmsHandy(phone.getNumber());
-		list.remove(phone);
+		Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+		alert.setTitle("Confirmation");
+		alert.setHeaderText("Do you really want to delete this phone?");
+		alert.setResizable(false);
+		alert.setContentText("Select okay to confirm deletion");
+		Optional<ButtonType> result = alert.showAndWait();
+		if(result.get() == ButtonType.OK) {
+			provider.removeSmsHandy(phone.getNumber());
+			list.remove(phone);
+		}
+		else if(result.get() == ButtonType.CANCEL) {
+		}
 	}
 
 
